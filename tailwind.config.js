931dbd361dc222589319182da7aba6a04cc2dc40
MariannaBr <brezinova.marianna@gmail.com>
2020-09-07 09:03:37 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '1/2': '50%',
+        '2/3': '66.666667%',
+      },
+    },
   },
   variants: {},
   plugins: [],
